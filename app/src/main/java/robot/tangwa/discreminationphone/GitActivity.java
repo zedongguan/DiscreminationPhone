677f51eb6c0e5f93d6619eca0dc98849c3a995d4
zedongguan <zedong_guan@foxmail.com>
2017-06-11 22:57:03 +0800

    guanzedong改变--关泽东

--- app/src/main/java/robot/tangwa/discreminationphone/GitActivity.java
+++ app/src/main/java/robot/tangwa/discreminationphone/GitActivity.java
@@ -3,12 +3,13 @@ package robot.tangwa.discreminationphone;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by Administrator on 2017/6/11.
  */
 
-public class GitActivity extends Activity {
+public class GitActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
