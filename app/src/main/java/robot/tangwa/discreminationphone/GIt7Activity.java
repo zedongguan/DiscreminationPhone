7ae1ac7feaa0d1fa39afeace66bfcdd3bbce70bd
zedongguan <zedong_guan@foxmail.com>
2017-06-12 00:43:50 +0800

    guanzedong---Git7Activity增加

---
+++ app/src/main/java/robot/tangwa/discreminationphone/GIt7Activity.java
@@ -0,0 +1,18 @@
+package robot.tangwa.discreminationphone;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.view.menu.ActionMenuItem;
+
+/**
+ * Created by Administrator on 2017/6/12.
+ */
+
+public class GIt7Activity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
