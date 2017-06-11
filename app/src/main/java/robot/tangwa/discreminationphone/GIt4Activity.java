486959ae499f60f7e2e09079abf3cb5aedc1b776
关泽东 <1181330466@qq.com>
2017-06-12 00:34:38 +0800

    Merge branch 'master' of https://github.com/zedongguan/DiscreminationPhone into guanzedong1
    # Conflicts:
    #	app/src/main/java/robot/tangwa/discreminationphone/MainActivity.java
    guanzedong1分支增加Git4Activity AppComp

--- app/src/main/java/robot/tangwa/discreminationphone/GIt4Activity.java
+++ app/src/main/java/robot/tangwa/discreminationphone/GIt4Activity.java
@@ -3,12 +3,13 @@ package robot.tangwa.discreminationphone;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by 关泽东 on 2017/6/12.
  */
 
-public class GIt4Activity extends Activity {
+public class GIt4Activity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
